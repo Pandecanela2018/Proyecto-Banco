b19a7b9e60a800328c88a1d79c3c0390aec9d6d5
Pandecanela2018 <javier.canesa2014@gmail.com>
2024-04-16 09:33:39 -0600

    prueba

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@
 
             if (ResCorrecC != Contraseña || ResCorrecU != Usuario)
             {
-                Console.Clear();
+                Console.Clear(); //jjajjskkeollep
                 Console.WriteLine("--------------------------------------------");
                 Console.WriteLine("Usuario o Contraseña es incorrecto.");
                 Console.WriteLine("Ingrese nuevamente su usuario y contraseña");
